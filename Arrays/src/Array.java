1e7b0b80f53b0326141206dce621a5a8d463216c
Jonas Ribeiro <krnsjrr@gmail.com>
2025-06-12 14:44:59 -0300

    Arrays methods

--- Arrays/src/Array.java
+++ Arrays/src/Array.java
@@ -1,33 +1,37 @@
 public class Array<T>{
-    private int size;
+    private int capacity, currentSize;
     private T[] vector;
 
 
-    public Array(int size) {
-        if(size < 0) throw new IllegalArgumentException();
-        this.vector = (T[]) new Object[size];
+
+    public Array(int capacity) {
+        if(capacity < 0) throw new IllegalArgumentException();
+        this.vector = (T[]) new Object[capacity];
+        this.currentSize = 0;
     }
+
     public boolean add(T element){
-        for(int i = 0; i < vector.length; i++ ){
-            if(vector[i] == null){
-                vector[i] = element;
-                return true;
-            }
+        if(currentSize < capacity){
+        vector[currentSize] = element;
+        currentSize++;
+        return true;
         }
         return false;
     }
+
     public boolean addAtPosition(int index, T element){
 
         if(index <= vector.length-1){
-            for(int i = 0; i < vector.length; i++ ){
-                if(index == i){
-                    vector[i] = element;
-                    return true;
+            for(int i = currentSize; i > index; i-- ){
+                vector[i] = vector[i-1];
                 }
+            vector[index] = element;
+            currentSize++;
+            return true;
             }
-        }
         return false;
-    }
+        }
+
 
     public int findIndexByValue(T element){
         for(int i = 0; i < vector.length; i++ ){
@@ -36,6 +40,22 @@ public class Array<T>{
             }
         }
         return -1;
+    }
+
+    public boolean removeByIndex(int index){
+        if(!(index < 0 || index >= currentSize)){
+            for(int i = index; i < currentSize; i++ ){
+                vector[i] = vector[i +1];
+            }
+            currentSize--;
+            return true;
+        }
+        return false;
+
+    }
+
+    public boolean removeByElementAll(T element){
+
     }
 
 }
