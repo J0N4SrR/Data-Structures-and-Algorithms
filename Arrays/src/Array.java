3e2d933442d87802e29421cb131f852aedc7570d
Jonas Ribeiro <krnsjrr@gmail.com>
2025-06-12 13:09:25 -0300

    Arrays methods

--- Arrays/src/Array.java
+++ Arrays/src/Array.java
@@ -4,15 +4,38 @@ public class Array<T>{
 
 
     public Array(int size) {
+        if(size < 0) throw new IllegalArgumentException();
         this.vector = (T[]) new Object[size];
     }
     public boolean add(T element){
-        for(T e : vector){
-            if(e == null){
-                e = element;
+        for(int i = 0; i < vector.length; i++ ){
+            if(vector[i] == null){
+                vector[i] = element;
                 return true;
             }
         }
-        return false;       
+        return false;
     }
+    public boolean addAtPosition(int index, T element){
+
+        if(index <= vector.length-1){
+            for(int i = 0; i < vector.length; i++ ){
+                if(index == i){
+                    vector[i] = element;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public int findIndexByValue(T element){
+        for(int i = 0; i < vector.length; i++ ){
+            if(vector[i].equals(element)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
 }
