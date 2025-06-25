23bfb26dc08b680d8a22c4129ab75bcc23202520
Jonas Ribeiro <krnsjrr@gmail.com>
2025-06-25 10:14:18 -0300

    add Insertion Sort

--- Arrays/src/Arrays.java
+++ Arrays/src/Arrays.java
@@ -138,8 +138,13 @@ public class Arrays<T extends Comparable<T>>  {
         return null;
     }
 
+    /* Bubble Sort, Selection Sort, and Insertion Sort
+    All three are comparison-based sorting algorithms, simple to understand and implement,
+    but considered inefficient for large data volumes due to their worst-case time complexity of O(N^2).
+    However, the way they reach this complexity and the operations they prioritize vary significantly. */
+
     //Bubble Sort
-    public void bubbleSort() throws Exception {
+    public void bubbleSort() {
         if(isEmpty() || currentSize < 2) return;
 
         for(int i = 0; i < currentSize - 1; i++){
@@ -153,7 +158,7 @@ public class Arrays<T extends Comparable<T>>  {
         }
     }
     //Selection Sort
-    public void selectionSort() throws Exception {
+    public void selectionSort()  {
         if(isEmpty() || currentSize < 2) return;
         T valueTemp;
         for(int i = 0; i < currentSize -1 ; i++){
@@ -168,7 +173,27 @@ public class Arrays<T extends Comparable<T>>  {
             vector[i] = valueTemp;
 
         }
-    }   
+    }
+
+    //Insertion Sort
+    public void insertionSort(){
+        if(isEmpty() || currentSize < 2) return;
+        T valueTemp;
+        for(int i = 1; i < currentSize; i++){
+            valueTemp = vector[i];
+            int j = i - 1;
+            while (j >= 0 && vector[j].compareTo(valueTemp) > 0){
+                vector[j+1] = vector[j];
+                j--;
+            }
+            vector[j+1] = valueTemp;
+        }
+    }
+
+    //Quicksort
+    public void quickSort(){
+
+    }
 
     @Override
     public String toString() {
